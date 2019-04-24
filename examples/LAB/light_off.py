e418a3887b068a4daf81d2917c88543188197746
jumbo Hsieh <jumbokh@gmail.com>
2019-04-24 10:42:50 +0800

    Add files via upload

---
+++ examples/LAB/light_off.py
@@ -0,0 +1,2 @@
+from machine import Pinimport timep2 = Pin(15, Pin.OUT)
+p2.value(0)time.sleep(1)
---
+++ examples/LAB/light_on.py
@@ -0,0 +1,2 @@
+from machine import Pinimport timep2 = Pin(15, Pin.OUT)
+p2.value(1)time.sleep(1)
