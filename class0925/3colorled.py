724ddddb763ab0ccba8bb67c860d640ba419355d
jumbo Hsieh <jumbokh@gmail.com>
2022-09-27 16:01:41 +0800

    Update 3colorled.py

--- class0925/3colorled.py
+++ class0925/3colorled.py
@@ -4,8 +4,8 @@ import clist
 
 frequency = 5000
 red = PWM(Pin(15), frequency)
-green = PWM(Pin(2), frequency)
-blue = PWM(Pin(4), frequency)
+green = PWM(Pin(4), frequency)
+blue = PWM(Pin(16), frequency)
 #clist = [[255,255,0],[0,255,255],[255,0,255],[104,42,67],[102,98,133]]
 
 while True:
