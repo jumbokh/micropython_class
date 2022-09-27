07b29c2c593364bdeb7b85ec5336b51555f4b08e
jumbo Hsieh <jumbokh@gmail.com>
2022-09-27 15:34:09 +0800

    Update RYGLED.py

--- class0925/RYGLED.py
+++ class0925/RYGLED.py
@@ -6,8 +6,16 @@ p2 = Pin(2, Pin.OUT)
 p4 = Pin(4, Pin.OUT)
 
 while True:
+    # 開綠燈 5 秒
+    p15.value(1)
+    time.sleep(5)
+    # 關綠燈，並開黃燈 1 秒
     p15.value(0)
+    p2.value(1)
     time.sleep(1)
-    P15.value(1)
-    time.sleep(1)
-p15.value(0)
+    # 關黃燈，並開紅燈 3 秒
+    P2.value(0)
+    p4.value(1)
+    time.sleep(3)
+    # 關紅燈
+    p4.value(0)
