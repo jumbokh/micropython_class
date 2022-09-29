d9af54246aff327b91d9456652ef199dded294e4
jumbo Hsieh <jumbokh@gmail.com>
2022-09-29 21:00:52 +0800

    Update cnt.py

--- class0925/cnt.py
+++ class0925/cnt.py
@@ -12,5 +12,5 @@ def connect_wifi(ssid, passwd):
     print("network config:", sta.ifconfig())
 
 SSID = config.SSID        # WiFi名稱
-PASSWORD = config.KEY    # WiFi密碼
+PASSWORD = config.PASSWORD    # WiFi密碼
 connect_wifi(SSID, PASSWORD)
